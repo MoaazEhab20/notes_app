48b833b1ddd35c5d688b6141065682c4ed9f9b78
abc <abc@example.com>
2023-08-26 02:36:52 +0300

    notes view body and custom app bar,search icon

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        SizedBox(
+          height: 50,
+        ),
+        Text(
+          'Notes',
+          style: TextStyle(fontSize: 28),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
